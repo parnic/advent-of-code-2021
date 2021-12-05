b8848ab334e7316e418c9737af33717e6556c027
Parnic <github@parnic.com>
2021-12-05 11:18:28 -0600

    C# day template

---
+++ src/Template.cs
@@ -0,0 +1,29 @@
+﻿namespace aoc2021
+{
+    internal class DayTemplate
+    {
+        internal static void Go()
+        {
+            Logger.Log("Day #");
+            Logger.Log("-----");
+            var lines = File.ReadAllLines("inputs/##.txt");
+            Part1(lines);
+            Part2(lines);
+            Logger.Log("");
+        }
+
+        private static void Part1(IEnumerable<string> lines)
+        {
+            using var t = new Timer();
+
+            Logger.Log($"part1: ");
+        }
+
+        private static void Part2(IEnumerable<string> lines)
+        {
+            using var t = new Timer();
+
+            Logger.Log($"part2: ");
+        }
+    }
+}
