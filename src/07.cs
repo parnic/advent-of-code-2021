cfb3c668c709efc76e1dfd24a2eb2881fe6854ac
Parnic <github@parnic.com>
2021-12-07 07:56:11 -0600

    Day 7 solution
    I'm reasonably happy with this one. I'm sure there's an easier way to determine the smallest distance, at least for part 1, but this works fast enough for me.

---
+++ src/07.cs
@@ -0,0 +1,59 @@
+﻿namespace aoc2021
+{
+    internal class Day07
+    {
+        internal static void Go()
+        {
+            Logger.Log("Day 7");
+            Logger.Log("-----");
+            var nums = File.ReadAllText("inputs/07.txt").Split(',').Select(int.Parse);
+            Part1(nums);
+            Part2(nums);
+            Logger.Log("");
+        }
+
+        private static (long, int) Solve(IEnumerable<int> nums, Func<long, long> formula)
+        {
+            var min = nums.Min();
+            var max = nums.Max();
+            long minDist = 0;
+            int? minNum = null;
+            for (int i = min; i <= max; i++)
+            {
+                long dist = 0;
+                foreach (var num in nums)
+                {
+                    dist += formula(Math.Abs(num - i));
+                }
+
+                if (minNum == null || dist < minDist)
+                {
+                    minDist = dist;
+                    minNum = i;
+                }
+            }
+
+            return (minDist, minNum.Value);
+        }
+
+        private static void Part1(IEnumerable<int> nums)
+        {
+            using var t = new Timer();
+
+            var (minDist, minNum) = Solve(nums, (d) => d);
+
+            Logger.Log($"part1: position: {minNum}, fuel cost: {minDist}");
+        }
+
+        private static void Part2(IEnumerable<int> nums)
+        {
+            using var t = new Timer();
+
+            // summation formula from https://en.wikipedia.org/wiki/Summation
+            // found by searching "factorial but with addition" because i'm smart like that.
+            var (minDist, minNum) = Solve(nums, (d) => ((d*d)+d)/2);
+
+            Logger.Log($"part2: position: {minNum}, fuel cost: {minDist}");
+        }
+    }
+}
--- src/main.cs
+++ src/main.cs
@@ -2,3 +2,4 @@ aoc2021.Day02.Go();
 aoc2021.Day03.Go();
 aoc2021.Day05.Go();
 aoc2021.Day06.Go();
+aoc2021.Day07.Go();
