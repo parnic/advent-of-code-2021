03c6ab60efae3b97ecb0f0ee48a3924aa0e9fc8f
Parnic <github@parnic.com>
2021-12-10 23:51:57 -0600

    Update template
    I found out that C# 10 added file-scoped namespaces that don't need the whole thing to be wrapped in braces, so that's neat.

--- src/Template.cs
+++ src/Template.cs
@@ -1,29 +1,28 @@
-﻿namespace aoc2021
+﻿namespace aoc2021;
+
+internal class DayTemplate
 {
-    internal class DayTemplate
+    internal static void Go()
     {
-        internal static void Go()
-        {
-            Logger.Log("Day #");
-            Logger.Log("-----");
-            var lines = File.ReadAllLines("inputs/##.txt");
-            Part1(lines);
-            Part2(lines);
-            Logger.Log("");
-        }
+        Logger.Log("Day #");
+        Logger.Log("-----");
+        var lines = File.ReadAllLines("inputs/##.txt");
+        Part1(lines);
+        Part2(lines);
+        Logger.Log("");
+    }
 
-        private static void Part1(IEnumerable<string> lines)
-        {
-            using var t = new Timer();
+    private static void Part1(IEnumerable<string> lines)
+    {
+        using var t = new Timer();
 
-            Logger.Log($"part1: ");
-        }
+        Logger.Log($"part1: ");
+    }
 
-        private static void Part2(IEnumerable<string> lines)
-        {
-            using var t = new Timer();
+    private static void Part2(IEnumerable<string> lines)
+    {
+        using var t = new Timer();
 
-            Logger.Log($"part2: ");
-        }
+        Logger.Log($"part2: ");
     }
 }
